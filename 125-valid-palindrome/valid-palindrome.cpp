e98d373a2592fa942dbcbcde949fd1d27357dfce
devmehrotra-git <mehrotra.dev07@gmail.com>
2026-08-08 11:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (54.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
+//one way
+    /*bool isPalindrome(string s) {
         string ss="";
         for(char i:s)
         {
@@ -14,6 +15,23 @@ public:
         {
             if(ss[i]!=ss[n-i-1])
             return false;
+        }
+        return true;
+    }*/
+
+    //two pointer
+    bool isPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j)
+        {
+            while(i<j and !isalnum(s[i]))
+            i++;
+            while(i<j and !isalnum(s[j]))
+            j--;
+            if(tolower(s[i])!=tolower(s[j]))
+            return false;
+            i++;
+            j--;
         }
         return true;
     }
