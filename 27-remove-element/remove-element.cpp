6f844f48ba578d05eb5706fdae39b0f90f955109
devmehrotra-git <mehrotra.dev07@gmail.com>
2026-08-29 09:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.36%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==val)
+            {
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return nums.size();
+        for(int i=j+1;i<nums.size();i++)
+        {
+            if(nums[i]!=val)
+            {
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        return j;
+    }
+};
